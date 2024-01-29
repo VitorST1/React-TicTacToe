c9b41a8ea1f9ad4db48bbefed76b5a8759ac278a
Vitor <vitorturqueti@hotmail.com>
2024-01-29 11:23:27 -0300

    square value start empty and when clicked, changes to X

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react"
 import Square from "./Square"
 
 export default function Board() {
+	const [squares, setSquares] = useState<string[]>(Array(9).fill(""))
+
+	const handleClick = (i: number) => {
+		const nextSquares = squares.slice()
+		nextSquares[i] = "X"
+		setSquares(nextSquares)
+	}
+
 	return (
 		<div className="grid w-fit grid-cols-3">
-			{[...Array(9)].map((_, i) => (
-				<Square key={i} value={(i + 1).toString()} />
+			{squares.map((value, i) => (
+				<Square key={i} value={value} onSquareClick={() => handleClick(i)} />
 			))}
 		</div>
 	)
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,6 +1,9 @@
-export default function Square(props: { value: string }) {
+export default function Square(props: { value: string; onSquareClick: () => void }) {
 	return (
-		<button className="h-9 w-9 border border-slate-400 bg-slate-50 text-2xl/6 font-bold">
+		<button
+			className="h-9 w-9 border border-slate-400 bg-slate-50 text-2xl/6 font-bold"
+			onClick={props.onSquareClick}
+		>
 			{props.value}
 		</button>
 	)
