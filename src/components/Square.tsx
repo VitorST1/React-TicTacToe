23fd0501fbdb76fee7ced6801c06987fb62a75c7
Vitor <vitorturqueti@hotmail.com>
2024-05-25 17:45:31 -0300

    added inset ring when focusing on square

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -6,7 +6,7 @@ export default function Square(props: {
 }) {
 	return (
 		<button
-			className={`h-20 w-20 border border-slate-400 text-5xl/9 font-bold sm:h-40 sm:w-40 sm:text-8xl ${props.winner && "text-green-500"}`}
+			className={`h-20 w-20 border border-slate-400 text-5xl/9 font-bold focus:outline-none focus:ring-4 focus:ring-inset focus:ring-slate-50 sm:h-40 sm:w-40 sm:text-8xl ${props.winner && "text-green-500"}`}
 			aria-label={`square${props.index + 1}`}
 			onClick={props.onSquareClick}
 		>
